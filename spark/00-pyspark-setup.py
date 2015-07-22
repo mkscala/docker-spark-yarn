52c02a48b0688672db779bbc4b9145ee0994817a
root <sergei.0910@gmail.com>
2015-07-22 19:56:52 +0000

    spark 1.4 script

--- spark/00-pyspark-setup.py
+++ spark/00-pyspark-setup.py
@@ -5,6 +5,14 @@ import sys
 spark_home = os.environ.get('SPARK_HOME', None)
 sys.path.insert(0, spark_home + "/python")
 
+# If Spark V1.4.x is detected, then add ' pyspark-shell' to
+# the end of the 'PYSPARK_SUBMIT_ARGS' environment variable
+spark_release_file = spark_home + "/RELEASE"
+if os.path.exists(spark_release_file) and "Spark 1.4" in open(spark_release_file).read():
+  pyspark_submit_args = os.environ.get("PYSPARK_SUBMIT_ARGS", "")
+  if not "pyspark-shell" in pyspark_submit_args: pyspark_submit_args += " pyspark-shell"
+  os.environ["PYSPARK_SUBMIT_ARGS"] = pyspark_submit_args
+
 # Add the py4j to the path.
 # You may need to change the version number to match your install
 sys.path.insert(0, os.path.join(spark_home, 'python/lib/py4j-0.8.2.1-src.zip'))
